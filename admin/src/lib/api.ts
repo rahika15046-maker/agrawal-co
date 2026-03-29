2c978bd60a5b4245f4529bde20fa65a04d3753b6
rahika15046-maker <rahika15046@gmail.com>
2026-03-29 13:15:25 +0530

    fix api url

--- admin/src/lib/api.ts
+++ admin/src/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL:"https://agrawal-co-production.up.railway.app/api",
   withCredentials: true,
 });
 api.interceptors.request.use((config) => {
